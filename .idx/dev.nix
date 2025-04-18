6376fc82bf9e4aa408423a1e4e327abf02b05043
luisnguyen2k9 <luisnguyen2k9@gmail.com>
2025-04-18 14:41:43 +0000

    Your descriptive commit message

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,6 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.openssh
     pkgs.nodejs_20
     pkgs.yarn
     pkgs.nodePackages.pnpm
